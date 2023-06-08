0a3c461a28223266d7faf9c47d66c6bc9d8df1e4
Shivam Suhane <59939145+suhaneshivam@users.noreply.github.com>
2023-06-09 03:16:39 +0530

    Update main.py
    remove print statements

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ class Prompt(BaseModel):
 
 @app.post("/generate_audio")
 def text_to_audio(prompt:Prompt):
-    print(prompt)
-    print(type(prompt))
-    print(hasattr(prompt, "text"))
-    print(prompt.text)
     text_prompt = prompt.text
     audio_array = gen_audio(text_prompt)
     return {"audio_array" : audio_array.tolist()}
+  
